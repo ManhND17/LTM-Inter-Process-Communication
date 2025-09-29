f10abfba436d8e4728bf84f3c3513653c44e4ef0
Thu Hà Nguyễn <nguyenthuhatq1952004@example.com>
2025-09-29 08:47:27 +0000

    Add logging

--- ICP_shared_memory_main.py
+++ ICP_shared_memory_main.py
@@ -2,13 +2,15 @@ import multiprocessing as mp
 from multiprocessing import shared_memory
 import numpy as np
 import time, os
+import logging
 
-
-def producer(shm_name, shape, count, finished, cond, num_messages=10):
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]  %(message)s", handlers=[logging.FileHandler("/home/hnguyen/LTM---Inter-Process-Communication/log/shared_memory.log", mode="w"), logging.StreamHandler()])
+def producer(shm_name, shape, count, finished, cond, num_messages=5):
     shm = shared_memory.SharedMemory(name=shm_name)
     buf = np.ndarray(shape, dtype=np.int32, buffer=shm.buf)
 
     for i in range(num_messages):
+        # data = np.random.randint(0, 100, size=shape, dtype=np.int32)
         data = np.arange(100, dtype=np.int32) + i
         with cond:
             buf[:] = data
@@ -37,13 +39,14 @@ def consumer(shm_name, shape, count, finished, cond, cid=1):
             data = buf.copy()
             last_seen = count.value
         data_mean = process_data(data)
-        print(f"Consumer {cid} đọc message {last_seen}, mean={data_mean:.1f}")
+        logging.info(f"Consumer {cid} đọc message {last_seen}, mean={data_mean:.1f}")
 
     shm.close()
 
 
 def process_data(data):
-    print(f"Processing data with mean {data.mean()}")
+    logging.info(f"Data received: {data}")
+    logging.info(f"Processing data with mean {data.mean()}")
     return data.mean()
 
 
@@ -54,7 +57,6 @@ def main():
 
     lock = mp.Lock()
     cond = mp.Condition(lock)
-    # count là biến dùng chung, i = int
     count = mp.Value("i", 0)
     finished = mp.Value("i", 0)
 
@@ -62,9 +64,9 @@ def main():
     c1 = mp.Process(target=consumer, args=(shm.name, shape, count, finished, cond, 1))
     c2 = mp.Process(target=consumer, args=(shm.name, shape, count, finished, cond, 2))
 
-    p.start()
     c1.start()
     c2.start()
+    p.start()
     p.join()
     c1.join()
     c2.join()
